bf56fb4eae3db865fa383f35f55f08147aa3b2b2
Vladimir Vasiliev <vladvasiliev52@gmail.com>
2024-06-08 13:15:43 +0300

    fixed: test_settings.py

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,8 +1,8 @@
 from api_yamdb import settings
+from django.test import override_settings
 
 
 class TestSettings:
-
+    @override_settings(DEBUG=False)
     def test_settings(self):
-
         assert not settings.DEBUG, 'Проверьте, что DEBUG в настройках Django выключен'
