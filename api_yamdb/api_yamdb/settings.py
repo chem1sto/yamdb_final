a4a4c9f4ee41269d056caa2542cd72fdc6f23353
Vladimir Vasiliev <vladvasiliev52@gmail.com>
2024-06-20 18:12:32 +0300

    added: settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,7 +4,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env.db'))
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
